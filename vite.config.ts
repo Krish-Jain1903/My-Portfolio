8dcce53114f2a8fed934a455a65cd763431a2f26
KrishJain <krishjain1903@gmail.com>
2025-05-14 03:25:33 +0530

    Base Name Chnages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/My-Portfolio/", // <-- Add this line
+  base: "/My-Portfolio/", // 👈 EXACTLY this (must match your repo name)
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
